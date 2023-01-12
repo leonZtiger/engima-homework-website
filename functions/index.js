c38331fbdb542452b2773773dde8265807fc2c45
leonZtiger <leon.bynander@gmail.com>
2023-01-12 10:25:47 -0500

    removde dead code

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,117 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
 
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
-//  app.use(myMiddleware);
+const memberships = {
+ gold:{
+   maxInvocationsPerMonth: 50
+  },
+  silver:{
+    maxInvocationsPerMonth: 20
+  },
+  free:{
+    maxInvocationsPerMonth: 10
+  }
+}
 
+//text generation callback
 app.get("/generateText", (req, res) => {
-  const uid = req.uid;
+  const userDoc = admin.firestore().doc("users/" + req.uid);
+
+  if (!userDoc) {
+    res.send("user doesnt exist").status(403);
+    return;
+  }
+
+  const user = userDoc.get().then((doc) => {
+    return doc;
+  })
+  var userPrivleges = getPrivlege(user.uid);
+
+  if(!userPrivleges){
+    res.send("membership doesnt exist").status(403);
+  }
+
+  const genType = req.type;
+  
+
   //  const text = req.text;
-  res.json({uid: uid, text: "bruh"}).send().status(200);
+  res.json({ uid: uid, text: "bruh" }).send().status(200);
 });
+
+function TextWrite(specs){
+
+}
+function TextReWrite(specs){
+
+}
+function TextParaphrase(specs){
+
+}
+
+function getPrivlege(uid) {
+  const userDoc = admin.firestore().doc("userMemberShips/" + uid);
+
+  if (!userDoc) {
+    return false;
+  }
+
+  const user = userDoc.get().then((doc) => {
+    return doc;
+  })
+
+  switch (user.membership) {
+    case ("free"):
+      return {
+        maxwordlength: 200,
+        specimens: 2,
+        avaibleInvocations: user.avaibleInvocations,
+      }
+     
+
+    case ("silver"):
+      return {
+        maxwordlength: 1000,
+        specimens: 5,
+        avaibleInvocations: user.avaibleInvocations,
+      }
+
+    case ("gold"):
+      return {
+        maxwordlength: 2000,
+        specimens: 5,
+        avaibleInvocations: user.avaibleInvocations,
+      }
+     
+    default:
+      return false;
+      
+  }
+}
+
+// when user created create document for that user
+exports.createUserProfile = functions.auth.user().onCreate((user) => {
+
+  const userdata = {
+    uid: user.uid,
+    documents: {
+
+    },
+    email: user.email,
+    settings: {
+
+    },
+    membership: 'free'
+  };
+
+  admin.firestore().doc('users/' + user.uid).set(userdata)
+
+})
+
+
 
 exports.api = functions.https.onRequest(app);
--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -11,9 +11,23 @@ import { GrTextAlignFull, GrTextAlignCenter, GrTextAlignRight } from 'react-icon
 import { AiFillInfoCircle } from "react-icons/ai";
 import { RxDoubleArrowRight } from 'react-icons/rx'
 
-function Editor(props){
+function Editor(props) {
 
   const { user } = useContext(userContext)
+
+  const [loading, setLoading] = useState(false)
+  const [downloadAmount, setdownload] = useState(10)
+  const [specimens, setSpecimens] = useState(["awd", "dawd", "daw"])
+  const [useSelect, setuseSelect] = useState(false)
+  const [showSpecs, setShowSpecs] = useState(false)
+  const types = [{ type: "Selecte type" }, { type: "Paraphrase" }, { type: "Writer" }, { type: "Rewriter" }]
+  const [selected, setSelected] = useState(types[0].type)
+  const [fontSize, setfontSize] = useState(12)
+  const [wordlength, setwordlength] = useState(200)
+
+  const [creativity, setCreativity] = useState(50)
+  var maxLength = 200;
+  var url = "";
 
   function generateSpecimens() {
     setLoading(true)
@@ -22,33 +36,33 @@ function Editor(props){
       method: 'get',
       url: 'https://us-central1-enigma-homework.cloudfunctions.net/api/generateText',
       data: {
-        text: "one chuck chuked chuks",
+        text: {
+          wholetext: props.text,
+          selectedText: props.highlight.text,
+          highlightStart: props.highlight.start,
+          highlightEnd: props.highlight.end,
+        },
+        settings: {
+          length: wordlength,
+          type: selected,
+          randomness:creativity,
+        },
         uid: user.uid
       }
     }).then((response) => {
 
       console.log(response.data.text, response.data.uid)
-
+      
+      setSpecimens(response.specimens)
+      
       setLoading(false)
       alert("Writing is completed!")
     })
       .catch((error) => {
-        console.log(error)
+        alert(error)
       })
   }
 
-  const [loading, setLoading] = useState(false)
-  const [downloadAmount, setdownload] = useState(10)
-  const [specimens, setSpecimen] = useState(["awd", "dawd", "daw"])
-  const [showSpecimen, setshowSpecimen] = useState(false)
-  const [chosenSpecimen, setChosenSpecimen] = useState("bruh")
-  const [useSelect, setuseSelect] = useState(false)
-  const [showSpecs, setShowSpecs] = useState(false)
-
-  const [fontSize, setfontSize] = useState(12)
-  const [creativity, setCreativity] = useState(50)
-  var maxLength = 200;
-  var url = "";
 
   function setInputVal(e, variable) {
     if (e.target.type = "number")
@@ -56,34 +70,53 @@ function Editor(props){
         variable(89)
       else
         variable(parseInt(e.target.value));
-
   }
 
+  function setSpecToText(text) {
+
+    if (!confirm("Specimen text will be added to text ")) {
+      return
+    }
+  }
   useEffect(() => {
 
-    if (useSelect)
-      return;
+    if (useSelect) {
+      // add selection button
+    }
 
 
   }, [useSelect])
 
+  useEffect(() => {
+    console.log(fontSize)
+    document.getElementById("text").style.fontSize = `${fontSize}px`
+    document.getElementById("textarea").style.fontSize = `${fontSize}px`
+    const typebar = document.getElementById("typebar")
+    if (typebar)
+      typebar.style.height = `${fontSize}px`
+
+  }, [fontSize])
+
+
+
   return (
     <>
+      {/*
       <div className={classes.filter} style={{ display: showSpecimen ? "block" : "none" }} />
       <div className={classes.showExample} style={{ display: showSpecimen ? "flex" : "none" }}>
         <p>
           {chosenSpecimen}
         </p>
         <div className={classes.btnCon}>
-          <button onClick={() => {  props.setText(chosenSpecimen);  setshowSpecimen(false) }}>Use</button>
-          <button onClick={() =>  setshowSpecimen(false)}>cancel</button>
+          <button onClick={() => { props.setText(chosenSpecimen); setshowSpecimen(false) }}>Use</button>
+          <button onClick={() => setshowSpecimen(false)}>cancel</button>
         </div>
       </div>
-
+        */}
       <div className={classes.container}>
-        
+
         <h1>Edit panel</h1>
-        
+
         <ul className={classes.editList}>
 
 
@@ -98,9 +131,9 @@ function Editor(props){
 
           <li className={classes.editItem}>
             <h3>Text align</h3>
-            <GrTextAlignFull onClick={(event) => {document.getElementById("text").style.textAlign = "left";document.getElementById("textarea").style.textAlign = "left"}} className={classes.icon} />
-            <GrTextAlignCenter onClick={(event) => {document.getElementById("text").style.textAlign = "center";document.getElementById("textarea").style.textAlign = "center"}} className={classes.icon} />
-            <GrTextAlignRight onClick={(event) => {document.getElementById("text").style.textAlign = "right";document.getElementById("textarea").style.textAlign = "right"}} className={classes.icon} />
+            <GrTextAlignFull onClick={(event) => { document.getElementById("text").style.textAlign = "left"; document.getElementById("textarea").style.textAlign = "left" }} className={classes.icon} />
+            <GrTextAlignCenter onClick={(event) => { document.getElementById("text").style.textAlign = "center"; document.getElementById("textarea").style.textAlign = "center" }} className={classes.icon} />
+            <GrTextAlignRight onClick={(event) => { document.getElementById("text").style.textAlign = "right"; document.getElementById("textarea").style.textAlign = "right" }} className={classes.icon} />
             <AiFillInfoCircle className={classes.infoIcon} />
             <p className={classes.infotext}>Set the alignment of your text</p>
           </li>
@@ -109,83 +142,90 @@ function Editor(props){
         <div className={classes.bottomCon}>
 
           <div className={classes.btnCon}>
-            <button onClick={()=>setShowSpecs(!showSpecs)} className={classes.openSpecs}>
+            <button onClick={() => setShowSpecs(!showSpecs)} className={classes.openSpecs}>
               Generate text
             </button>
             {
-              showSpecs?
-                 <div className={classes.generateSpecs}>
-              <h1>generation specs
-                <RxDoubleArrowRight onClick={()=>setShowSpecs(!showSpecs)} className={classes.icon} />
-              </h1>
+              showSpecs ?
+                <div className={classes.generateSpecs}>
+                  <h1>generation specs
+                    <RxDoubleArrowRight onClick={() => setShowSpecs(!showSpecs)} className={classes.icon} />
+                  </h1>
 
-              <div className={classes.editItem}>
-                <h3>
-                  use selected text</h3>
-                <div className={classes.switchCon}>
-                  <input value={useSelect ? "on" : "off"} onClick={(e) => { setuseSelect(!useSelect); console.log(useSelect) }} className={classes.switchInput} type="checkbox" />
-                  <span className={classes.slider}></span>
-                </div>
+                  <div className={classes.editItem}>
+                    <h3>
+                      use selected text</h3>
+                    <div className={classes.switchCon}>
+                      <input value={useSelect ? "on" : "off"} onClick={(e) => { setuseSelect(!useSelect); console.log(useSelect) }} className={classes.switchInput} type="checkbox" />
+                      <span className={classes.slider}></span>
+                    </div>
 
 
-                <AiFillInfoCircle className={classes.infoIcon} />
-                <p className={classes.infotext}>When used a whole new text will be regenerated. If not the AI will keep writing forward of the text</p>
+                    <AiFillInfoCircle className={classes.infoIcon} />
+                    <p className={classes.infotext}>When used a whole new text will be regenerated. If not the AI will keep writing forward of the text</p>
 
-              </div>
-
-              <div className={classes.editItem}>
-                <label for="creativity" className={classes.editLabel}>
-                  creativity</label>
-                <h4>{creativity}</h4>
-                <input value={creativity} onChange={(e) => setCreativity(e.target.value)} step={"10"} type={"range"} id="creativity" name="creativity" />
-                <AiFillInfoCircle className={classes.infoIcon} />
-                <p className={classes.infotext}>How creative the AI will be when writing. 0 equals very low and 100 equals super random. 50 is recommended for starters</p>
-
-              </div>
-              <div className={classes.editItem}>
-                <label for="maxlength" className={classes.editLabel}>
-                  max word-length</label>
-                <input type={"number"} value={fontSize} id="maxlength" name="maxlength" onChange={(e) => setInputVal(e, setfontSize)} />
-                <AiFillInfoCircle className={classes.infoIcon} />
-                <p className={classes.infotext}>Max word count when generating</p>
-
-              </div>
-
-              <div className={classes.editItem}>
-                <h3>
-                  Generation type
-                </h3>
-                <div className={classes.genType}>
-                  <button className={classes.selected}>Paraphrase</button>
-                  <div className={classes.dropdown}>
-                    <button>Paraphrase</button>
-                    <button>a</button>
-                    <button>Paraphrase</button>
                   </div>
-                </div>
+
+                  <div className={classes.editItem}>
+                    <label for="creativity" className={classes.editLabel}>
+                      creativity</label>
+                    <h4>{creativity}</h4>
+                    <input value={creativity} onChange={(e) => setCreativity(e.target.value)} step={"10"} type={"range"} id="creativity" name="creativity" />
+                    <AiFillInfoCircle className={classes.infoIcon} />
+                    <p className={classes.infotext}>How creative the AI will be when writing. 0 equals very low and 100 equals super random. 50 is recommended for starters</p>
+
+                  </div>
+                  <div className={classes.editItem}>
+                    <label for="maxlength" className={classes.editLabel}>
+                      max word-length</label>
+                    <input type={"number"} value={wordlength} id="maxlength" name="maxlength" onChange={(e) => setInputVal(e, setwordlength)} />
+                    <AiFillInfoCircle className={classes.infoIcon} />
+                    <p className={classes.infotext}>Max word count when generating</p>
+
+                  </div>
+
+                  <div className={classes.editItem}>
+                    <h3>
+                      Generation type
+                    </h3>
+                    <div className={classes.genType}>
+                      <button className={classes.selected}>{selected}</button>
+                      <div className={classes.dropdown}>
+
+                        {
+                          types.map((type) => {
+
+                            if (type.type != selected)
+                              return (<button onClick={() => setSelected(type.type)}>{type.type}</button>)
+                          })
+
+                        }
+
+                      </div>
+                    </div>
 
 
-                <AiFillInfoCircle className={classes.infoIcon} />
-                <p className={classes.infotext}>When used a whole new text will be regenerated. If not the AI will keep writing forward of the text</p>
+                    <AiFillInfoCircle className={classes.infoIcon} />
+                    <p className={classes.infotext}>When used a whole new text will be regenerated. If not the AI will keep writing forward of the text</p>
 
-              </div>
+                  </div>
 
-              <button onClick={() => generateSpecimens()} className={classes.genBtn}>
-                Generate text
-                <TfiReload className={loading ? classes.loadingIcon : classes.icon} />
-              </button>
+                  <button onClick={() => generateSpecimens()} className={classes.genBtn}>
+                    Generate text
+                    <TfiReload className={loading ? classes.loadingIcon : classes.icon} />
+                  </button>
 
-            </div>:
-            null
+                </div> :
+                null
             }
-         
+
           </div>
 
           <h1>specimens</h1>
           <div className={classes.specimensCons}>
             {
               specimens.map((text, index) => (
-                <div onMouseLeave={()=>props.setdisplaySpecimenText(undefined)} onMouseOver={()=> props.setdisplaySpecimenText(text)} onClick={() => setshowSpecimen(true)} className={classes.specimen}>
+                <div onClick={() => setSpecToText(text)} onMouseLeave={() => props.setdisplaySpecimenText(undefined)} onMouseOver={() => props.setdisplaySpecimenText(text)} className={classes.specimen}>
                   <p>{text}</p>
                 </div>
               ))
