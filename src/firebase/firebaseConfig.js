e4ba2da3e0e73e3a1ae412346f9ece08ba4a6d6c
leon <98460339+leonZtiger@users.noreply.github.com>
2023-01-19 12:18:38 +0100

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,7 +7,7 @@ import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBQ9s14iH1HCKZJebodpMex2kS7Bxyx_SE",
+    apiKey: "",
 
     authDomain: "enigma-homework.firebaseapp.com",
 
@@ -17,9 +17,9 @@ const firebaseConfig = {
 
     messagingSenderId: "593199837146",
 
-    appId: "1:593199837146:web:57fe7e583875dac2c49ae3",
+    appId: "",
 
-    measurementId: "G-KX28074L9K"
+    measurementId: ""
 
 };
 
